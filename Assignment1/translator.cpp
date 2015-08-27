63ba7f17d68ec911e4acf7bba9e8434074febf60
R-Droid <ritesh.iet11@gmail.com>
2015-08-27 11:13:01 +0530

    changing asm and code

--- Assignment1/translator.cpp
+++ Assignment1/translator.cpp
@@ -33,34 +33,55 @@ typedef pair<int, int > pii;
 int main()
 {
 	std::ios::sync_with_stdio(false);
-	freopen ("G:\\Assignments\\Semester3\\CloudComputing\\Assignment1\\add_32.asm","r",stdin);
-	freopen ("G:\\Assignments\\Semester3\\CloudComputing\\Assignment1\\add_64_out.asm","w",stdout);
+	freopen ("32_bit.asm","r",stdin);
+	freopen ("64_bit_conv.asm","w",stdout);
 	string s;
+	bool first = true;
 	getline(cin, s);
+	cout<<"extern printf"<<"\n";
 	while(true)
 	{
-	    if(s.find(".note.GNU-stack") != std::string::npos)
-            break;
-		if(s.find("pushl") != std::string::npos)
-			s.replace(s.find("pushl"), 5,"pushq");
-		if(s.find(" 8") != std::string::npos)
-			s.replace(s.find(" 8"), 2," 16");
-		if(s.find("-8") != std::string::npos)
-			s.replace(s.find("-8"), 2,"-16");
-		if(s.find(" 5") != std::string::npos)
-			s.replace(s.find(" 5"), 2," 6");
-		if(s.find("movl") != std::string::npos)
-			s.replace(s.find("movl"), 4,"movq");
-		if(s.find("%e") != std::string::npos)
+	    if(s.find("	int 80h") != std::string::npos && !first)
+        {
+        	s.replace(s.find("	int 80h"), 8, "ret");
+        	break;	
+        }
+		
+		if(s.find("	hello:     db 'Hello world',10") != std::string::npos)
+			s.replace(s.find("	hello:     db 'Hello world',10"), 31,"msg:	db 'Hello world', 0");
+		
+		if(s.find("helloLen:") != std::string::npos)
+			s.replace(s.find("helloLen:"), 22,"fmt:    db \"%s\", 10, 0");
+		
+		if(s.find("	global _start") != std::string::npos)
+			s.replace(s.find("	global _start"), 14,"        global main");
+		
+		if(s.find("_start:") != std::string::npos)
+			s.replace(s.find("_start:"), 7,"main:");
+		
+		if(s.find("	mov eax,4") != std::string::npos)
+			s.replace(s.find("	mov eax,4"), 10,"        push    rbp");
+		
+		if(s.find("	mov ebx,1") != std::string::npos)
+			s.replace(s.find("	mov ebx,1"), 10, "	mov	rdi,fmt");
+
+		if(s.find("	mov ecx,hello") != std::string::npos)
+			s.replace(s.find("	mov ecx,hello"), 14, "	mov	rsi,msg");
+
+		if(s.find("	mov edx,helloLen") != std::string::npos)
+			s.replace(s.find("	mov edx,helloLen"), 17, "	mov	rax,0");
+		
+		if(s.find("	int 80h") != std::string::npos && first)
 		{
-			s.replace(s.find("%e"), 2, "%r");
-			if(s.find("%e") != std::string::npos)
-				s.replace(s.find("%e"), 2, "%r");
+			s.replace(s.find("	int 80h"), 8, "        call    printf");
+			first = false;
 		}
-		if(s.find("addl") != std::string::npos)
-			s.replace(s.find("addl"), 4, "addq");
-		if(s.find("subl") != std::string::npos)
-			s.replace(s.find("subl"), 4,"subq");
+
+		if(s.find("	mov eax,1") != std::string::npos)
+			s.replace(s.find("	mov eax,1"), 10, "pop	rbp");
+
+		if(s.find("	mov ebx,0") != std::string::npos)
+			s.replace(s.find("	mov ebx,0"), 10, "	mov	rax,0");
 
 		cout<<s<<"\n";
 		getline(cin, s);
